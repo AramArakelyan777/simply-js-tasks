0e74144b8e77a33b59a555ee2ca3ebbcc9dfe309
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-03-21 22:34:16 +0400

    get a random opponent function and caesar decicion with random boolean

--- javascript/task2.js
+++ javascript/task2.js
@@ -1,16 +1,14 @@
 import { faker } from "@faker-js/faker"
 
-const numberOfGladiators = Math.floor(Math.random() * 15) + 15
+const numberOfGladiators = Math.floor(Math.random() * 6) + 5
 
 function Gladiator() {
     this.name = faker.person.fullName()
     this.health = Math.floor(Math.random() * 21) + 80
-    this.power = Math.floor(Math.random() * 4) + 2
-    this.speed = Math.floor(Math.random() * 5) + 1
-}
-
-function caesarsDecision() {
-    return Math.random() >= 0.5
+    this.initialHealth = this.health
+    this.power = (Math.floor(Math.random() * 31) + 20) / 10
+    this.initialSpeed = (Math.floor(Math.random() * 5001) + 1000) / 1000
+    this.speed = this.initialSpeed
 }
 
 const gladiators = []
@@ -18,4 +16,13 @@ for (let i = 0; i < numberOfGladiators; i++) {
     gladiators.push(new Gladiator())
 }
 
-export function task2() {}
+function caesarsDecision() {
+    return Math.random() >= 0.5
+}
+
+function getRandomOpponent(attacker) {
+    let opponents = gladiators.filter((g) => g !== attacker && g.health > 0)
+    return opponents.length > 0
+        ? opponents[Math.floor(Math.random() * opponents.length)]
+        : null
+}
